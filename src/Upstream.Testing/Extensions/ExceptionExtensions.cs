4bf3947648aa5fbb98553c849da30933f04d0ca2
Tom McKinney <tom@upstreamcode.com>
2020-02-28 17:42:24 -0500

    Simplify project dependencies (#2)

---
+++ src/Upstream.Testing/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Upstream.Testing.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static void ThrowIfAny(this IEnumerable<Exception> exceptions)
+        {
+            var count = exceptions?.Count();
+
+            if (count > 0)
+            {
+                if (count == 1)
+                {
+                    throw exceptions.First();
+                }
+                else
+                {
+                    throw new AggregateException(exceptions);
+                }
+            }
+        }
+    }
+}
--- src/Upstream.Testing/TestBase.cs
+++ src/Upstream.Testing/TestBase.cs
@@ -1,18 +1,24 @@
 ﻿using Moq;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
+using Upstream.Testing.Extensions;
 
 namespace Upstream.Testing
 {
     public abstract class TestBase<T> : IDisposable
         where T : class
     {
-        protected abstract T CreateTestClass();
-
         protected virtual bool VerifyAll => true;
+
+        protected virtual MockRepository MockRepository { get; } = new MockRepository(MockBehavior.Strict);
+
+        protected abstract T CreateTestClass();
 
         protected virtual void VerifyMocks()
         {
+            var exceptions = new List<Exception>();
+
             Type instanceType = this.GetType();
 
             var allFields = instanceType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
@@ -24,9 +30,30 @@ namespace Upstream.Testing
                     string verifyMethodName = VerifyAll ? nameof(Mock.VerifyAll) : nameof(Mock.Verify);
                     MethodInfo verifyMethod = field.FieldType.GetMethod(verifyMethodName, new Type[] { });
 
-                    verifyMethod?.Invoke(field.GetValue(this), new object[] { });
+                    try
+                    {
+                        verifyMethod?.Invoke(field.GetValue(this), new object[] { });
+                    }
+                    catch (Exception e)
+                    {
+                        exceptions.Add(e);
+                    }
                 }
             }
+
+            try
+            {
+                if (VerifyAll)
+                    MockRepository?.VerifyAll();
+                else
+                    MockRepository?.Verify();
+            }
+            catch (Exception e)
+            {
+                exceptions.Add(e);
+            }
+
+            exceptions.ThrowIfAny();
         }
 
         public void Dispose()
